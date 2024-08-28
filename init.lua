883d1817f5bafa75095fc2e2ca9148a7f4e619d8
David Peng <dzp@andrew.cmu.edu>
2024-08-28 14:29:26 -0400

    add svlangserver

--- init.lua
+++ init.lua
@@ -552,9 +552,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -564,6 +564,18 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
+
+        svlangserver = {
+          settings = {
+            systemverilog = {
+              includeIndexing = { '**/*.{sv,svh}' },
+              excludeIndexing = {},
+              defines = {},
+              launchConfiguration = '/tools/verilator -sv -Wall --lint-only',
+              formatCommand = '/tools/verible-verilog-format',
+            },
+          },
+        },
 
         lua_ls = {
           -- cmd = {...},
@@ -717,12 +729,12 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
+          -- ['<Tab>'] = cmp.mapping.select_next_item(),
           --['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
